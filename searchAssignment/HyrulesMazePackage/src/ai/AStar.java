3a4b46beb384e4e0414d97c30e7908016fca8ef1
Guilherme Taschetto <gtaschetto@gmail.com>
2014-08-28 18:47:41 -0300

    Refactory.

--- searchAssignment/HyrulesMazePackage/src/ai/AStar.java
+++ searchAssignment/HyrulesMazePackage/src/ai/AStar.java
@@ -1,7 +1,6 @@
 package ai;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Queue;
--- searchAssignment/HyrulesMazePackage/src/ai/ISearch.java
+++ searchAssignment/HyrulesMazePackage/src/ai/ISearch.java
@@ -1,7 +1,5 @@
 package ai;
 
-import java.util.Comparator;
-
 public interface ISearch {
 	Node Search(Problem problem);
 }
