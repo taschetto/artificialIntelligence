6f773d517c96120d8584d1169ba490f72ff55c95
Guilherme Taschetto <gtaschetto@gmail.com>
2014-08-30 22:32:34 -0300

    Update MiniMaxPlayer and AlphaBetaPlayer classes.

--- adversarialSearch/TicTacToePackage/src/ai/AlphaBetaPlayer.java
+++ adversarialSearch/TicTacToePackage/src/ai/AlphaBetaPlayer.java
@@ -1,5 +1,7 @@
 package ai;
 
+import java.util.ArrayList;
+
 public class AlphaBetaPlayer extends MiniMaxPlayer {
 
 	public AlphaBetaPlayer(int playerIndex) {
@@ -8,15 +10,91 @@ public class AlphaBetaPlayer extends MiniMaxPlayer {
 	}
 
 	@Override
-	public Move getNextMove(State state) throws Exception {
-		//TODO Your code here
-		return null;
-	}
-	
-
-	@Override
 	public int getPlayerType() {
 		return ALPHABETA_PLAYER;
 	}
+	
+	@Override
+	public Move getNextMove(State state) throws Exception {
+		Move move = null;
+		int max = Integer.MIN_VALUE;
+		for (Move a : Actions(state)) {
+			int utility = MinValue(Results(state, a), Integer.MIN_VALUE, Integer.MAX_VALUE);
+			if (utility > max) {
+				move = a;
+				max = utility;
+			}
+		}
+		return move;
+	}
+
+	public int MaxValue(State state, int alpha, int beta) throws Exception
+	{
+		if (TerminalTest(state)) return Utility(state);
+		
+		int v = Integer.MIN_VALUE;
+		for (Move a : Actions(state))
+		{
+			v = Math.max(v, MinValue(Results(state, a), alpha, beta));
+			if (v >= beta) return v;
+			alpha = Math.max(alpha, v);
+		}
+		
+		return v;
+	}
+	
+	public int MinValue(State state, int alpha, int beta) throws Exception
+	{
+		if (TerminalTest(state)) return Utility(state);
+		
+		int v = Integer.MAX_VALUE;
+		for (Move a : Actions(state))
+		{
+			v = Math.min(v, MaxValue(Results(state, a), alpha, beta));
+			if (v <= alpha) return v;
+			beta = Math.min(beta, v);
+		}
+		
+		return v;
+	}
+	
+	public Move[] Actions(State state) throws Exception
+	{
+		ArrayList<Move> actions = new ArrayList<Move>();
+		for	(int row = 0; row < State.SIZE; row++)
+			for (int col = 0; col < State.SIZE; col++)
+				if (state.getPlayerIndexOfSquare(row, col) == State.PLAYER_NONE)
+					actions.add(new Move(row, col));
+				
+		return actions.toArray(new Move[actions.size()]);		
+	}
+	
+	public State Results(State state, Move move) throws Exception
+	{
+		State newState = (State)state.clone();
+		int player = state.getTurn();
+		int other = player == State.PLAYER_X ? State.PLAYER_O : State.PLAYER_X;
+		newState.setState(move.row, move.col, player);
+		newState.setTurn(other);
+		return newState;
+	}
+	
+	public int Utility(State state) throws Exception
+	{
+		int player = this.playerIndex == State.PLAYER_X ? State.PLAYER_X : State.PLAYER_O;
+		int other = this.playerIndex == State.PLAYER_X ? State.PLAYER_O : State.PLAYER_X;
+		
+		if (state.isWin(player))
+			return 1;
+		else if (state.isWin(other))
+			return -1;
+		else
+			return 0;
+	}
+	
+	public boolean TerminalTest(State state) throws Exception
+	{
+		return state.isGameOver();
+	}	
 
 }
--- adversarialSearch/TicTacToePackage/src/ai/MiniMaxPlayer.java
+++ adversarialSearch/TicTacToePackage/src/ai/MiniMaxPlayer.java
@@ -12,41 +12,32 @@ public class MiniMaxPlayer implements Player {
 	}
 
 	@Override
-	public Move getNextMove(State state) throws Exception {
-		//TODO Your code here
-		return null;
-	}
-
-	@Override
 	public int getPlayerType() {
 		return MINIMAX_PLAYER;
 	}
-	
-	private int Max(int a, int b)
-	{
-		return a > b ? a : b;
-	}
-	
-	private int Min(int a, int b)
-	{
-		return a < b ? a : b;
-	}	
 
-	public Move MinimaxDecision(State state)
-	{
-		return null;
+	@Override
+	public Move getNextMove(State state) throws Exception {
+		Move move = null;
+		int max = Integer.MIN_VALUE;
+		for (Move a : Actions(state)) {
+			int utility = MinValue(Results(state, a));
+			if (utility > max) {
+				move = a;
+				max = utility;
+			}
+		}
+		return move;
 	}
-	
+
 	public int MaxValue(State state) throws Exception
 	{
-		if (this.TerminalTest(state))
-			return this.Utility(state);
+		if (TerminalTest(state)) return Utility(state);
 		
 		int v = Integer.MIN_VALUE;
-		
-		for (Move move : this.Actions(state))
+		for (Move a : Actions(state))
 		{
-			v = Max(v, MinValue(Results(state, move)));
+			v = Math.max(v, MinValue(Results(state, a)));
 		}
 		
 		return v;
@@ -54,42 +45,46 @@ public class MiniMaxPlayer implements Player {
 	
 	public int MinValue(State state) throws Exception
 	{
-		if (this.TerminalTest(state))
-			return this.Utility(state);
+		if (TerminalTest(state)) return Utility(state);
 		
 		int v = Integer.MAX_VALUE;
-		
-		for (Move move : this.Actions(state))
+		for (Move a : Actions(state))
 		{
-			v = Min(v, MaxValue(Results(state, move)));
+			v = Math.min(v, MaxValue(Results(state, a)));
 		}
 		
 		return v;
 	}
 	
-	public Move[] Actions(State state)
+	public Move[] Actions(State state) throws Exception
 	{
-		ArrayList<Move> moves = new ArrayList<Move>();
-		for	(int row = 0; row < 3; row++)
-			for (int col = 0; col < 3; col++)
-				if (state.getState(row, col).equals(State.BLANK))
-					moves.add(new Move(row, col));
+		ArrayList<Move> actions = new ArrayList<Move>();
+		for	(int row = 0; row < State.SIZE; row++)
+			for (int col = 0; col < State.SIZE; col++)
+				if (state.getPlayerIndexOfSquare(row, col) == State.PLAYER_NONE)
+					actions.add(new Move(row, col));
 				
-		return moves.toArray(new Move[moves.size()]);		
+		return actions.toArray(new Move[actions.size()]);		
 	}
 	
 	public State Results(State state, Move move) throws Exception
 	{
 		State newState = (State)state.clone();
-		newState.setState(move.row, move.col, state.getTurn());
+		int player = state.getTurn();
+		int other = player == State.PLAYER_X ? State.PLAYER_O : State.PLAYER_X;
+		newState.setState(move.row, move.col, player);
+		newState.setTurn(other);
 		return newState;
 	}
 	
 	public int Utility(State state) throws Exception
 	{
-		if (state.isWin(State.PLAYER_X))
+		int player = this.playerIndex == State.PLAYER_X ? State.PLAYER_X : State.PLAYER_O;
+		int other = this.playerIndex == State.PLAYER_X ? State.PLAYER_O : State.PLAYER_X;
+		
+		if (state.isWin(player))
 			return 1;
-		else if (state.isWin(State.PLAYER_O))
+		else if (state.isWin(other))
 			return -1;
 		else
 			return 0;
